4379990ba86a349d4785bdbef2a8f58e4660d040
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-07 13:35:32 -0600

    Configured: localStorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,22 +3,32 @@ import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 import { types } from '../types/types';
 
-const initialState = {
-  logged: false,
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return {
+    logged: !!user,
+    user,
+  }
 }
 
 export const AuthProvider = ({ children }) => {
 
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = '') => {
+
+    const user = {
+      id: 1,
+      name
+    }
+
     const action = {
       type: types.login,
-      payload: {
-        id: 1,
-        name
-      }
+      payload: user,
     }
+
+    localStorage.setItem('user', JSON.stringify(user));
     dispatch(action);
   }
 
