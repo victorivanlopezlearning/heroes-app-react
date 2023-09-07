5d452d06ada4b61af0bc746ee711b1ac3a99c29f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-07 14:57:13 -0600

    Created: PrivateRoute

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,11 +2,15 @@ import { Navigate } from 'react-router-dom';
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
 import { LoginPage } from '../auth';
 import { HeroesLayout } from '../heroes/pages/HeroesLayout';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = [
   {
     path: '/',
-    element: <HeroesLayout />,
+    element:
+      <PrivateRoute>
+        <HeroesLayout />
+      </PrivateRoute>,
     children: HeroesRoutes,
   },
   {
