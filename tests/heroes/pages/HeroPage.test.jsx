8c3965fa6c32649d6a61a79e55c2085cbddd97df
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-13 18:11:23 -0600

    Finished: tests

--- tests/heroes/pages/HeroPage.test.jsx
+++ tests/heroes/pages/HeroPage.test.jsx
@@ -1,16 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { HeroPage } from '../../../src/heroes/pages/HeroPage';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import { MarvelPage } from '../../../src/heroes/pages';
 
-// jest.mock('react-router-dom', () => ({
-//   ...jest.requireActual('react-router-dom'),
-//   useNavigate: jest.fn(),
-// }));
+const mockUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+}));
 
 describe('Tests in <HeroPage />', () => {
 
-  // beforeEach(() => jest.clearAllMocks());
+  beforeEach(() => jest.clearAllMocks());
 
   test('should show a hero', () => {
 
@@ -19,7 +21,6 @@ describe('Tests in <HeroPage />', () => {
     render(
       <MemoryRouter initialEntries={[`/hero/${heroId}`]}>
         <Routes>
-          <Route path="marvel" element={<MarvelPage />} />
           <Route path="hero/:heroId" element={<HeroPage />} />
         </Routes>
       </MemoryRouter>
@@ -46,5 +47,24 @@ describe('Tests in <HeroPage />', () => {
     const heading = screen.getByRole('heading', { level: 1 });
 
     expect(heading.textContent).toBe('Marvel Comics');
+  });
+
+  test('should call navigate function if button clicked', () => {
+
+    const heroId = 'dc-batman';
+
+    render(
+      <MemoryRouter initialEntries={[`/hero/${heroId}`]}>
+        <Routes>
+          <Route path="hero/:heroId" element={<HeroPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    expect(mockUseNavigate).toHaveBeenCalled();
+    expect(mockUseNavigate).toHaveBeenCalledWith(-1);
   });
 });
